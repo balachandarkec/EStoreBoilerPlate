0b362d811cfe45d1ca8119233e00d916faa52ebd
Balachandar <balachandar.s@niit.com>
2021-06-09 11:00:59 +0530

    cart bilerplate Added

---
+++ src/main/java/in/stack/eStore/model/Order.java
@@ -0,0 +1,13 @@
+package in.stack.eStore.model;
+
+import java.time.LocalDateTime;
+
+public class Order {
+	
+	private int orderId;
+	private LocalDateTime orderDate;
+	private int status;
+	private String customerName;
+
+
+}
---
+++ src/main/java/in/stack/eStore/model/OrederDetails.java
@@ -0,0 +1,10 @@
+package in.stack.eStore.model;
+
+import java.util.List;
+
+public class OrederDetails {
+ 
+  private List<Product> products;
+  private double discount;
+  
+}
